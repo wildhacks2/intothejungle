46a98aaec6cb90b6bce6961d7a7c6a455f819b45
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2022-08-07 17:09:02 +0800

    Link up pages

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 L.marker([-3.4647054431875395, -62.21605220740857])
   .addTo(map)
   .bindPopup(
-    "Amazon Rainforest <br> Impact of Human Activity on Rainforests"
+    "<div class='popup'>Amazon Rainforest <br> Impact of Human Activity on Rainforests<br><a href='./jungle3/main/jungle3.html'>Learn More</a></div>"
   )
 
 // add marker Daintree Rainforest
 L.marker([-16.164790459877185, 145.4189750188323])
   .addTo(map)
   .bindPopup(
-    "Daintree Rainforest <br> Learn about Endangered Animal Species and Animal Fun Facts"
+    "<div class='popup'>Daintree Rainforest <br> Learn about Endangered Animal Species and Animal Fun Facts<br><a href='./daintree/daintree.html'>Learn More</a></div>"
   )
 
 // add marker Amazon Rainforest
 L.marker([-1.801150808947209, 16.97109905499256])
   .addTo(map)
   .bindPopup(
-    "Congo Rainforest <br> Importance of Rainforest conservation"
+    "<div class='popup'>Congo Rainforest <br> Importance of Rainforest conservation<br><a href='./jungle3/main/jungle3.html'>Learn More</a></div>"
   )
