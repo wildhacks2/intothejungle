3f671fffbd297d08baf0cc237f757640f8f5ec2f
e0425705 <puahxiuwen@gmail.com>
2022-08-07 12:49:06 +0800

    add navigation between pages

---
+++ jungle3/main/jungle3.js
@@ -0,0 +1,15 @@
+function firstIssue() {
+    window.location.href = "../mining/mining.html";
+}
+
+function secondIssue() {
+    window.location.href = "../logging/logging.html";
+}
+
+function thirdIssue() {
+    window.location.href = "../ranching/ranching.html";
+}
+
+function fourthIssue() {
+    window.location.href = "../agriculture/agriculture.html";
+}
