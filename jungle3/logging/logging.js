4c61477d7aa6f9d99224f1c1c7a6a22c4b67fe9f
e0425705 <puahxiuwen@gmail.com>
2022-08-07 14:22:02 +0800

    finish on logging issue
    added animal images, names

--- jungle3/logging/logging.js
+++ jungle3/logging/logging.js
@@ -1,3 +1,7 @@
 function back() {
     window.location.href = "../main/jungle3.html";
 }
+
+function loggingThreat() {
+    window.location.href = "../logging/species.html";
+}
---
+++ jungle3/logging/species.js
@@ -0,0 +1,3 @@
+function back() {
+    window.location.href = "../main/jungle3.html";
+}
